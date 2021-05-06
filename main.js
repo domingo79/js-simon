d911b59d0ff309f61250a0b935d9ee0d38b57789
domingo79 <andria140779@yahoo.it>
2021-05-06 16:20:42 +0200

    numeri casuali

---
+++ main.js
@@ -0,0 +1,57 @@
+/*
+Descrizione:
+Un alert() espone 5 numeri generati casualmente.
+Da li parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+/*
+schema riassuntivo per le task
+1. generare 5 numeri casuali
+2. mostrare all'utente i numeri generati
+3. fare partire un countdown per l'utente
+4 passati i 30 sec l'utente deve inserire i numeri uno alla volta
+5. verificare i numeri indovinati
+*/
+
+
+//1- array dei 5 numeri casuali ottenuti da una funzione max min e controllo con ciclo while 
+
+var numeriCasuali = [];
+
+function numeroRandomn(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+while (numeriCasuali.length < 5) {
+
+    var numero = numeroRandomn(1, 3000)
+
+    if (!numeriCasuali.includes(numero)) {
+        numeriCasuali.push(numero);
+    }
+}
+console.log(numeriCasuali);
+//2- mostrare all'utente i numeri generati
+alert(`Prova a ricordare i numeri, 
+quando sarai pronto, fai click su OK.. 
+Inserisci i numeri uno alla volta...
+ECCO I NUMERI: => ${numeriCasuali} <=
+RICORDATI CHE HAI 30 SECONDI`)
+
+//3-fare partire un countdown per l'utente
+var secondi = 30;
+var countDownEle = document.getElementById('countDown');
+
+var intervallo = setInterval(function () {
+
+    if (secondi === 0) {
+        alert('Tempo scaduto... :-((');
+        clearInterval(intervallo);
+
+    } else {
+        secondi--;
+    } countDownEle.innerHTML = secondi;
+
+}, 1000);
+
