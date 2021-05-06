395965471dbb9e8ea90e8cd2f5736d58b47e44f9
domingo79 <andria140779@yahoo.it>
2021-05-06 22:09:19 +0200

    verifica numeri

--- main.js
+++ main.js
@@ -5,19 +5,15 @@ Da li parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
-/*
-schema riassuntivo per le task
-1. generare 5 numeri casuali
-2. mostrare all'utente i numeri generati
-3. fare partire un countdown per l'utente
-4 passati i 30 sec l'utente deve inserire i numeri uno alla volta
-5. verificare i numeri indovinati
-*/
 
 
-//1- array dei 5 numeri casuali ottenuti da una funzione max min e controllo con ciclo while 
+//task 1- array dei 5 numeri casuali ottenuti da una funzione max min e controllo con ciclo while 
 
 var numeriCasuali = [];
+var numeriUtente = [];
+var secondi = 3;
+var countDownEle = document.getElementById('countDown');
+
 
 function numeroRandomn(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -25,29 +21,27 @@ function numeroRandomn(min, max) {
 
 while (numeriCasuali.length < 5) {
 
-    var numero = numeroRandomn(1, 10)
+    var numero = numeroRandomn(1, 50)
 
     if (!numeriCasuali.includes(numero)) {
         numeriCasuali.push(numero);
     }
 }
-console.log('Numeri generati random: ' + numeriCasuali);
 
-//2- mostrare all'utente i numeri generati
-alert(`Prova a ricordare i numeri, 
-quando sarai pronto, fai click su OK.. 
-Inserisci i numeri uno alla volta...
+//task 2- mostrare all'utente i numeri generati
+
+alert(`Prova a ricordare i numeri,
+quando sarai pronto, fai click su OK..
+Ti lascerò riflettere per alcuni secondi...
 ECCO I NUMERI: => ${numeriCasuali} <=
-RICORDATI CHE HAI 30 SECONDI`)
+Ti chiederò di inserire i numeri a breve..`)
 
-//3-fare partire un countdown per l'utente
-var secondi = 5;
-var countDownEle = document.getElementById('countDown');
+//task 3-fare partire un countdown per l'utente
 
 var intervallo = setInterval(function () {
 
     if (secondi === 0) {
-        alert('Inserisci i numeri che ricordi');
+        alert('Pronto?? ricordi i numeri? Inserisci i numeri uno per volta..');
         clearInterval(intervallo);
     }
     else {
@@ -55,16 +49,32 @@ var intervallo = setInterval(function () {
     } countDownEle.innerHTML = secondi;
 
 }, 1000);
-//4- passati i 30 sec l'utente deve inserire i numeri uno alla volta tramite prompt()
 
-var numeriUtente = [];
+//task 4- passati i 30 sec l'utente deve inserire i numeri uno alla volta tramite prompt()
 
 setTimeout(function () {
     while (numeriUtente.length < 5) {
-        var numeroInserito = parseInt(prompt('inserisci il primo numero..'));
+        var numeroInserito = parseInt(prompt('Inserisci il numero..'));
         numeriUtente.push(numeroInserito);
-        console.log('numeri inseriti dall\'utente ' + numeriUtente);
     }
-}, 6000)
+    //task 5 verificare i 2 array
+    var numeriIndovinati = [];
+
+    for (var i = 0; i < 5; i++) {
+        var elemento = numeriUtente[i];
+        if (numeriCasuali.includes(elemento)) {
+            numeriIndovinati.push(elemento)
+        }
+
+    }
+    document.getElementById('winLose').innerHTML = `Hai indovinato ${numeriIndovinati.length}/di 5 numeri... ${numeriIndovinati}`;
+
+
+}, 4000);
+
+
+
+
+
 
 
